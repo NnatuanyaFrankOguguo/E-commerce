23c01b08db29833f303285ec5298cf85a8bb7139
Nnatuanya Frank <nnatuanyafrank@gmail.com>
2024-10-01 00:10:32 +0100

    vite-config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs'; // Add this
+import nodeResolve from '@rollup/plugin-node-resolve'; // Add this
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs(), // Include the CommonJS plugin
+    nodeResolve()], // Include the Node Resolve plugin],
+  
 })
