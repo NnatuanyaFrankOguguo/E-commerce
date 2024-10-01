d04c72f17c80231ac069dbc9ca9e1908b64f208a
Nnatuanya Frank <nnatuanyafrank@gmail.com>
2024-10-01 11:20:09 +0100

    error cleared

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar/navbar'
+import Navbar from './components/navbar/Navbar'
 import Sidebar from './components/sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import Listfoods from './pages/List/Listfoods'
